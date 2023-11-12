be87fe55be2e1796da092d64a021c9da613bc420
Soltyk Maksym <maxwell88soltyk@gmail.com>
2023-11-12 23:50:10 +0200

    save

--- packages/client/src/domains/chat/layouts/ChatLayout/ChatLayout.tsx
+++ packages/client/src/domains/chat/layouts/ChatLayout/ChatLayout.tsx
@@ -4,7 +4,7 @@ import styles from "./ChatLayout.module.scss";
 
 function ChatLayout({ children }: ChatLayoutProps) {
   return (
-    <div>
+    <div className={styles.layout}>
       <header className={styles.header}>
         <h1>Chat bots 2.0</h1>
       </header>
